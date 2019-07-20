1ab9fce6fec76628926aa63013fcae3872e88dec
franz lopez <franzelunico@gmail.com>
2019-07-20 18:07:08 -0400

    revisado

--- practica_tarea/practica_funciones/palabra_mas_larga.py
+++ practica_tarea/practica_funciones/palabra_mas_larga.py
@@ -1,16 +1,12 @@
+# ok
 palabra1 = input("Ingrese palabra1: ")
 palabra2 = input("Ingrese palabra2: ")
 
 def get_diferencia(palabra1,palabra2):
-    tam1 = len(palabra1)
-    tam2 = len(palabra2)
-
-    tam_dif = 0
-    if tam1>tam2:
-        tam_dif = tam1-tam2  
-    else:
-        palabra1,palabra2=palabra2,palabra1
-        tam_dif = tam2-tam1
+    tam_dif = len(palabra1) - len(palabra2)
+    if tam_dif < 0 :
+        palabra1, palabra2 = palabra2, palabra1
+        tam_dif *=-1
     print("La palabra %s tiene %i letras mas que %s." % (palabra1,tam_dif,palabra2))
 
 get_diferencia(palabra1,palabra2)
