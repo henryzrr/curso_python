012fa241b63d4061681e533391845a95acb3e9fd
franz lopez <franzelunico@gmail.com>
2019-07-20 18:21:22 -0400

    revisado

--- practica_tarea/practica_funciones/ej_6_numero_mayor_lista.py
+++ practica_tarea/practica_funciones/ej_6_numero_mayor_lista.py
@@ -1,4 +1,6 @@
+# ok
 from random import randint
+import time
 
 def mayor1(ls):
     ls.sort()
@@ -11,14 +13,30 @@ def mayor2(n,ls):
         return (ls[0] if ls[0]>n else n)
     else:
         return mayor2((ls[0] if ls[0]>n else n),ls[1:])
+
+def mayor3(lista):
+    return max(lista)
         
 
 ls = [randint(1,1000) for i in range(50)]
 print("La lista generada aleatoria usada para buscar el mayor es:\n")
 print(ls)
 print("\nAplicando la funcion 1 el numero mayor es:")
-print((mayor1(ls))[49])
+start = time.time()
+print((mayor1(ls))[49], end=' ')
+end = time.time()
+print('time: ', end -start)
+
 print("\nAplicando la funcion 2 el numero mayor es:")
-print((mayor2(ls[0],ls[1:])))
+start = time.time()
+print((mayor2(ls[0],ls[1:])), end=' ')
+end = time.time()
+print('time: ', end -start)
+
+print("\nAplicando la funcion 3 el numero mayor es:")
+start = time.time()
+print(mayor3(ls), end=' ')
+end = time.time()
+print('time: ', end -start)
 
 
