f48b6e288acd83dea3a41e617c5488265dcd2e2a
franz lopez <franzelunico@gmail.com>
2019-07-20 18:27:58 -0400

    revisado

--- practica_tarea/practica_funciones/ej_7_buscar_palabra.py
+++ practica_tarea/practica_funciones/ej_7_buscar_palabra.py
@@ -1,16 +1,17 @@
+# ok
 n = int(input("Cuantas palabras desea ingresar? :"))
 ls = [input() for i in range(n)]
 
 palabra=input("Ingrese la palabra que desea buscar: ")
 ls_res =[]
 for cad in ls:
-    if palabra in cad:
+    if palabra.upper() in cad.upper():
         ls_res.append(cad)
 
 print("\nLas coincidencias encontradas para '%s' con un for normal es:"%(palabra))
 print("res=",ls_res)
 
 print("\nLas coincidencias encontradas para '%s' con listas de comprension es:"%(palabra))
-ls_res2 = [cad for cad in ls if palabra in cad]
+ls_res2 = [cad for cad in ls if palabra.upper() in cad.upper()]
 print("res=",ls_res2)
 
